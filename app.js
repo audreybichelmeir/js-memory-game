f06044b4b1c10945922351b894c30d4954972ab1
audreybichelmeir <bichelmeir@chapman.edu>
2021-04-27 06:32:04 +0000

    flip card function

--- app.js
+++ app.js
@@ -52,6 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const grid = document.querySelector('.grid')
+  var cardsChosen = [] //arr cards chosen
+  var cardsChosenId = []
+
   //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -62,6 +65,43 @@ document.addEventListener('DOMContentLoaded', () => {
       grid.appendChild(card)
     }
   }
+
+
+
+  //check for matches
+function checkForMatch(){
+  var cards = documents.querSelectorAdd('img')
+  const optionOneId = cardsChosenId[0]
+  //this means wants the first value in the array
+  const optionTwoId = cardsChosenId[1]
+  if(cardChosen[0] == cardChosen[1]) {
+    alert('You found a match')
+     cards[optionOneId].setAttribute('src', 'images/white.png')
+     cards[optionTwoId].setAttribute('src', 'images/white.png')
+}
+
+
+
+
+  //flip card
+function flipCard(){
+  var cardId = this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name) //push the card base on their id
+  cardChosenId.push(cardId)
+  this.setAttribute('src', cardArray)
+  //card picken - let us add img to grid base on image id it holds
+  if(cardsChosen.length === 2){
+    setTimeout(checkForMatch, 500)
+
+
+  }
+
+
+
+}
+
+
+
 
   createBoard()
 })
